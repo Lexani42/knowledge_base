e77b27bfe28f379cec0d4979393dd5166101ed92
lexani42 <lexxani42@gmail.com>
2021-05-24 12:07:57 +0300

    add get_users func

--- app_web.py
+++ app_web.py
@@ -6,9 +6,12 @@ router = web.RouteTableDef()
 
 @router.get('/users/')
 async def get_users(request):
-    ...
+    users = {}
+    for user in User.select():
+        users[user.id] = user.name
+    return web.json_response(users)
 
 
 app = web.Application()
 app.add_routes(routes=router)
-web.run_app(app)
+web.run_app(app, host='localhost')
